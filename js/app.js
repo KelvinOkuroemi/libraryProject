7dc95e01ca1f8454b3d3fa7883ea055a0db6117b
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-10-05 20:24:28 +0100

    push new changes

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let titleInput = document.getElementById("title");
 let authorInput = document.getElementById("author");
 let pageInput = document.getElementById("pages")
 let submitButton = document.getElementById("submit-button");
+let deleteBtn = document.getElementById("delete-button")
 
 // @NTS => These form doesn't need a route to point to because we are manipulating the 
 // inputs and the elements with the DOM, so if the button is pressed, we use DOM to access the values.
@@ -27,6 +28,8 @@ class Book {
         this.author = author;
         this.pages = pages;
     }
+
+
 }
 
 // ***ADD to library***
@@ -34,19 +37,17 @@ function addToLibrary() {
     let title = titleInput.value;
     let author = authorInput.value;
     let pages = pageInput.value;
-
     let newBook = new Book(title, author, pages)
     library.push(newBook);
-    console.log(library)
 }
 
 // Display library on page
 function renderLibrary() {
-    library.forEach(book => {
-        console.log(book)
+    library.forEach((book,index) => {
         let newCard = document.createElement("div");
+        newCard.id = "book-card";
+        newCard.dataset.index = index;
         newCard.className = "book-card flex flex-col w-full justify-center border py-32 px-16 rounded-lg h-32";
-        
         newCard.innerHTML = `<div class="info-group flex-col flex space-y-2">
         <p id="title-card" class="w-full">${book.title}</p>
         <p id="author-card" class="w-full">${book.author}</p>
@@ -67,6 +68,7 @@ function renderLibrary() {
       </div>`
 
       boardOverlay.appendChild(newCard);
+      console.log(newCard.dataset.index);
     })
 }
 
@@ -85,3 +87,20 @@ submitButton.addEventListener("click", (e) => {
     })
 })
 
+// ***Delete
+
+deleteBtn.addEventListener("click",()=>{
+    const doDelete = confirm("You are about to delete this card")
+})
+
+// BUGS AND ISSUES
+// assign dataset to DOM elements
+// get index of array __ DONE
+// DOM cards' button not working
+// Read and unread
+
+// function loopLibrary(){
+//     library.forEach((book,index)=>{
+//         console.log(index)
+//     })
+// }
