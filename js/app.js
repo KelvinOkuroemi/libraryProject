78c35efc32cdfa759b0f641c2f952c4dc4c09905
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-10-08 13:28:18 +0100

    make delete functionality work

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@
 // Form inputs
 let titleInput = document.getElementById("title");
 let authorInput = document.getElementById("author");
-let pageInput = document.getElementById("pages")
+let pageInput = document.getElementById("pages");
 let submitButton = document.getElementById("submit-button");
-let deleteBtn = document.getElementById("delete-button")
+let deleteBtn = document.getElementById("delete-button");
+
 
 // @NTS => These form doesn't need a route to point to because we are manipulating the 
 // inputs and the elements with the DOM, so if the button is pressed, we use DOM to access the values.
@@ -19,40 +20,43 @@ let pageCard = document.getElementById("pages-card");
 const boardOverlay = document.getElementById("board")
 
 // ***Class to create new card info***
-
-const library = [];
+let library = [];
 
 class Book {
-    constructor(title, author, pages) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-    }
+  constructor(title, author, pages,id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.id = id;
+    this.checkbox = checkbox;
+  }
 
 
 }
 
 // ***ADD to library***
 function addToLibrary() {
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let pages = pageInput.value;
-    let newBook = new Book(title, author, pages)
-    library.push(newBook);
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = pageInput.value;
+  let id = library.length
+  let newBook = new Book(title, author, pages,id)
+  console.log(id);
+  library.push(newBook);
 }
 
 // Display library on page
 function renderLibrary() {
-    library.forEach((book,index) => {
-        let newCard = document.createElement("div");
-        newCard.id = "book-card";
-        newCard.dataset.index = index;
-        newCard.className = "book-card flex flex-col w-full justify-center border py-32 px-16 rounded-lg h-32";
-        newCard.innerHTML = `<div class="info-group flex-col flex space-y-2">
-        <p id="title-card" class="w-full">${book.title}</p>
-        <p id="author-card" class="w-full">${book.author}</p>
+  let i = library.length - 1;
+  let newCard = document.createElement("div");
+  newCard.id = "book-card";
+  newCard.className =
+    "book-card flex flex-col w-full justify-center border py-32 px-16 rounded-lg h-32";
+  newCard.innerHTML = `<div class="info-group flex-col flex space-y-2">
+        <p id="title-card" class="w-full">${library[i].title}</p>
+        <p id="author-card" class="w-full">${library[i].author}</p>
         <p id="pages-card" class="w-full">
-          <span id="number">${book.pages}</span>&nbsp;pages
+          <span id="number">${library[i].pages}</span>&nbsp;pages
         </p>
       </div>
       <div
@@ -61,46 +65,49 @@ function renderLibrary() {
         <button
           type="button"
           id="delete-button"
+          data-index=${library[i].id}
           class="bg-red-400 hover:bg-red-200 text-white font-bold py-2 px-4 rounded"
         >
           Delete
         </button>
-      </div>`
-
-      boardOverlay.appendChild(newCard);
-      console.log(newCard.dataset.index);
-    })
+      </div>`;
+  boardOverlay.appendChild(newCard);
 }
 
 submitButton.addEventListener("click", () => {
-    addToLibrary();
-    renderLibrary();
+  addToLibrary();
+  renderLibrary();
 })
 
 // Remove elements after loading
 submitButton.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const inputs = document.querySelectorAll("input");
-    inputs.forEach(input => {
-        input.value = ""
-    })
+  e.preventDefault();
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach(input => {
+    input.value = ""
+  })
 })
 
-// ***Delete
 
-deleteBtn.addEventListener("click",()=>{
-    const doDelete = confirm("You are about to delete this card")
+// ***Delete
+boardOverlay.addEventListener("click", (e) => {
+  const eventButtton = e.target.id;
+  if (eventButtton === "delete-button") {
+    const doDelete = confirm("You are about to delete this card");
+    if (doDelete) {
+      deleteCard(e)
+    }
+  }
 })
 
 // BUGS AND ISSUES
-// assign dataset to DOM elements
-// get index of array __ DONE
-// DOM cards' button not working
 // Read and unread
 
-// function loopLibrary(){
-//     library.forEach((book,index)=>{
-//         console.log(index)
-//     })
-// }
+function deleteCard(e) {
+  let button = e.target;
+  button.parentElement.parentElement.remove();
+  library.filter((book)=>{
+    console.log(button.dataset.index)
+    return button.dataset.index !== book.id;
+  })
+}
